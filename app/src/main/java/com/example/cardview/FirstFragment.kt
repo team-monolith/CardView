d604dac052eba2a8b394fb3c83acae00dc926603
COMPASS <team.monolith.2020@gmail.com>
2021-01-13 12:30:39 +0900

    ueda

--- app/src/main/java/com/example/cardview/FirstFragment.kt
+++ app/src/main/java/com/example/cardview/FirstFragment.kt
@@ -1,19 +1,17 @@
 package com.example.cardview
 
 import android.app.AlertDialog
-import android.content.DialogInterface
-import android.graphics.Rect
-import android.media.Image
+import android.content.Context.WINDOW_SERVICE
+import android.graphics.Point
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.view.*
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -41,10 +39,10 @@ class FirstFragment : Fragment() {
 
     //レイアウト追加用関数
     //Fragmentでの処理のためViewを引数として受け取る（findViewById等に利用するため）
-    fun layout_add(view: View){
+    fun layout_add(view: View) {
 
         //スクロールビューのifを取得しビューグループに変換
-        val SV:ViewGroup=view.findViewById<View>(R.id.sv) as ViewGroup
+        val SV: ViewGroup = view.findViewById<View>(R.id.sv) as ViewGroup
 
         //削除の場合はコメントを消す（後で調べなくていいように敢えて書き残します）
         //SV.removeAllViews()
@@ -53,30 +51,40 @@ class FirstFragment : Fragment() {
         getLayoutInflater().inflate(R.layout.cardpage, SV)
 
         //今回追加したレイアウトのデータを取得
-        val layout:ConstraintLayout=SV.getChildAt(count)as ConstraintLayout
+        val layout: ConstraintLayout = SV.getChildAt(count) as ConstraintLayout
 
         //レイアウトにタグをつけて後で扱いやすくする
         layout.setTag(count)
 
 
+            view.setOnClickListener {
+                val vTag=view.getTag()
+                if (vTag==1) {
+                    val iv = ImageView(context)
+                    iv.setImageResource(R.drawable.example2)
+                    val FriendImageDialog = AlertDialog.Builder(activity)
+                    FriendImageDialog.setTitle("test")
+                            .setMessage("名刺イメージ")
+                            .setView(iv)
+                            .setPositiveButton("OK") { _, _ ->
+                                Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show()
+                            }
+                            .setNegativeButton("キャンセル", null)
+                            .show()
+
+                }
+                //ディスプレイサイズの取得
+                /*val size = Point().also {
+                    (context.getSystemService(WINDOW_SERVICE) as WindowManager).defaultDisplay.apply { getSize(it) }
+                }
+                val width = size.x
+                val height = size.y*/
 
 
-        view.findViewById<ImageView>(R.id.imageView1).setOnClickListener {
-            val iv=ImageView(context)
-            iv.setImageResource(R.drawable.example2)
-            val FriendImageDialog=AlertDialog.Builder(activity)
-            FriendImageDialog.setTitle("test")
-                    .setMessage("名刺イメージ")
-                    .setView(iv)
-                    .setPositiveButton("OK"){ _, _ ->
-                        Toast.makeText(context,"OK",Toast.LENGTH_SHORT).show()
-                    }
-                    .setNegativeButton("キャンセル",null)
-                    .show()
-            Toast.makeText(context,"test",Toast.LENGTH_SHORT).show()
-        }
 
 
+                Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+            }
 
 
         //カウントを追加する
